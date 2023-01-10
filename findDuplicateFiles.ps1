64756d42bc5196ddf9dd5fee901264729e844173
human <36423947+noctacode@users.noreply.github.com>
2023-01-10 15:58:10 +0100

    Update findDuplicateFiles.ps1

--- findDuplicateFiles.ps1
+++ findDuplicateFiles.ps1
@@ -1,3 +1,5 @@
+<# this script finds files that are present in both locations#>
+
 $folder1 = Get-ChildItem -Recurse -path "C:\folderName"
 $folder2 = Get-ChildItem -Recurse -path "D:\otherFolderName"
 
